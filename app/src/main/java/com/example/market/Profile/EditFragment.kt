d2ce81043c4fef130804e2170d55933b5a2c8a52
estherP23 <123536492+estherP23@users.noreply.github.com>
2023-12-04 22:29:00 +0900

    Update EditFragment.kt

--- app/src/main/java/com/example/market/Profile/EditFragment.kt
+++ app/src/main/java/com/example/market/Profile/EditFragment.kt
@@ -15,24 +15,27 @@ import com.example.market.R
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 
-
 class EditFragment : Fragment() {
-    // Firestore 데이터베이스를 가져옵니다.
+
+    // Firebase Firestore 인스턴스 생성
     private lateinit var db: FirebaseFirestore
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        // fragment_edit_post 레이아웃을 인플레이트하여 뷰 생성
         val view = inflater.inflate(R.layout.fragment_edit_post, container, false)
 
-        // Fragment의 argument에서 productId를 가져오기
+        // 전달된 productId를 가져오기
         val productId = arguments?.getString("productId")
+        // Firebase Firestore 인스턴스 초기화
         db = FirebaseFirestore.getInstance()
 
-        // productId가 null이 아닌 경우
+        // 만약 productId가 null이 아니면 실행
         if (productId != null) {
-            //view 요소를 가져오기
+
+            // 뷰 요소 초기화
             val titleTextView = view.findViewById<TextView>(R.id.titleTextView)
             val titleEditText = view.findViewById<EditText>(R.id.titleEditText)
             val contentTextView = view.findViewById<TextView>(R.id.contentTextView)
@@ -43,27 +46,29 @@ class EditFragment : Fragment() {
             val isAvailableCheckBox = view.findViewById<CheckBox>(R.id.isAvailableCheckBox)
             val editButton = view.findViewById<Button>(R.id.editButton)
 
-            // Firestore에서 productId 가져오기
+            // Firebase Firestore에서 해당 productId의 데이터 가져오기
             db.collection("posts")
                 .document(productId)
                 .get()
                 .addOnSuccessListener { documentSnapshot ->
                     val product = documentSnapshot.toObject(ProductDetail::class.java)
                     if (product != null) {
-                        // 각 view 요소에 product의 정보를 대입
+                        // 제품 정보를 뷰에 설정
                         titleTextView.text = product.title
                         contentTextView.text = product.content
                         priceTextView.text = product.price.toString()
                         sellerTextView.text = product.seller
                         isAvailableCheckBox.isChecked = product.sell
 
+                        // 수정 버튼 클릭 리스너 설정
                         editButton.setOnClickListener {
-                            // 수정하기 클릭 시 editText에 현재 정보를 대입후, textView 숨기기
                             if (editButton.text == "수정하기") {
+                                // 수정하기 버튼 클릭 시, 편집 가능한 뷰로 변경
                                 titleEditText.setText(titleTextView.text.toString())
                                 contentEditText.setText(contentTextView.text.toString())
                                 priceEditText.setText(priceTextView.text.toString())
 
+                                // 뷰 가시성 설정
                                 titleEditText.visibility = View.VISIBLE
                                 contentEditText.visibility = View.VISIBLE
                                 priceEditText.visibility = View.VISIBLE
@@ -75,23 +80,25 @@ class EditFragment : Fragment() {
                                 priceTextView.visibility = View.GONE
 
                                 editButton.text = "수정완료"
-
                             } else if (editButton.text == "수정완료") {
-                                // editText에서 변경한 정보를 가져오기
+                                // 수정완료 버튼 클릭 시, 입력된 데이터로 게시물 업데이트
                                 val title = titleEditText.text.toString()
                                 val content = contentEditText.text.toString()
                                 val priceString = priceEditText.text.toString()
                                 val isAvailable = isAvailableCheckBox.isChecked
                                 val price = priceString.toIntOrNull()
 
+                                // 입력 값 유효성 검사
                                 if (title.isEmpty() || content.isEmpty() || price == null) {
                                     Toast.makeText(context, "모든 항목을 입력해주세요.", Toast.LENGTH_SHORT)
                                         .show()
                                     return@setOnClickListener
                                 }
-                                // 현재 로그인한 사용자의 이메일을 가져오기
+
+                                // 현재 사용자의 이메일 가져오기
                                 val sellerEmail = FirebaseAuth.getInstance().currentUser?.email
-                                // 변경한 정보 post에 전달
+
+                                // 수정된 게시물 데이터 생성
                                 val post = hashMapOf(
                                     "id" to productId,
                                     "title" to title,
@@ -100,12 +107,13 @@ class EditFragment : Fragment() {
                                     "sell" to isAvailable,
                                     "sellerEmail" to sellerEmail
                                 )
-                                //post 업데이트
+
+                                // Firebase Firestore에 수정된 데이터 업데이트
                                 db.collection("posts").document(productId)
                                     .update(post as Map<String, Any>)
                                     .addOnSuccessListener {
-                                        //editText를 숨기고, textView를 보이게 하기
-                                        Toast.makeText(context, "게시물이 수정되었습니다.", Toast.LENGTH_SHORT)
+                                        // 성공적으로 수정된 경우, Toast 메시지 표시 및 뷰 업데이트
+                                        Toast.makeText(context, "게시물이 수정 완료", Toast.LENGTH_SHORT)
                                             .show()
 
                                         titleEditText.visibility = View.GONE
@@ -124,6 +132,7 @@ class EditFragment : Fragment() {
                                         editButton.text = "수정하기"
                                     }
                                     .addOnFailureListener {
+                                        // 수정 실패 시 Toast 메시지 표시
                                         Toast.makeText(
                                             context,
                                             "게시물 수정 실패: ${it.message}",
@@ -135,6 +144,6 @@ class EditFragment : Fragment() {
                     }
                 }
         }
-        return view
+        return view // 생성된 뷰 반환
     }
 }
