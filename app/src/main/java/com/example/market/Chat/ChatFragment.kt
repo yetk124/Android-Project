6985b51ed9395ecd263c07e2c6a2c89f85e0a2c1
estherP23 <123536492+estherP23@users.noreply.github.com>
2023-12-04 22:19:21 +0900

    Update ChatFragment.kt

--- app/src/main/java/com/example/market/Chat/ChatFragment.kt
+++ app/src/main/java/com/example/market/Chat/ChatFragment.kt
@@ -15,8 +15,11 @@ import com.google.firebase.firestore.FirebaseFirestore
 
 class ChatFragment : Fragment() {
 
+    // Firebase Authentication 및 Firestore 인스턴스 변수 선언
     private lateinit var auth: FirebaseAuth
     private lateinit var db: FirebaseFirestore
+
+    // 채팅 목록을 표시하는 RecyclerView 및 어댑터 선언
     private lateinit var chatView: RecyclerView
     private lateinit var chatAdapter: ChatAdapter
 
@@ -24,43 +27,49 @@ class ChatFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        // 프래그먼트의 레이아웃을 확장(inflate)합니다.
         val view = inflater.inflate(R.layout.fragment_chat, container, false)
 
+        // Firebase 인스턴스 초기화
         auth = FirebaseAuth.getInstance()
         db = FirebaseFirestore.getInstance()
 
+        // RecyclerView 및 어댑터 초기화
         chatView = view.findViewById(R.id.messageView)
         chatAdapter = ChatAdapter(this, emptyList())
 
+        // RecyclerView에 LinearLayoutManager 및 어댑터 설정
         chatView.layoutManager = LinearLayoutManager(context)
         chatView.adapter = chatAdapter
 
+        // 메시지를 불러오는 함수 호출
         loadMessage()
         return view
     }
 
+    // Firestore에서 메시지를 불러와 RecyclerView에 표시하는 함수
     private fun loadMessage() {
-        //현재 로그인된 사용자의 이메일을 가져온다
+        // 현재 사용자 이메일 가져오기
         val currentUserEmail = FirebaseAuth.getInstance().currentUser?.email
         if (currentUserEmail != null) {
-            //db의 chat 콜렉션의 field에서 seller에 해당하는 이메일이 현재 로그인된 사용자의 이메일과 같다면 message 가져온다
+            // Firestore에서 "seller" 필드가 현재 사용자 이메일과 일치하는 채팅 목록을 가져옵니다.
             db.collection("chat")
-                .whereEqualTo(
-                    "seller",
-                    currentUserEmail
-                )
+                .whereEqualTo("seller", currentUserEmail)
                 .get()
                 .addOnSuccessListener { documents ->
                     val chats = mutableListOf<Chat>()
-                    //chat 컬렉션에 있는 message 모두 & db의 chat 콜렉션의 field에서 seller에 해당하는 이메일이 현재 로그인된 사용자의 이메일과 같다면 message 출력
-                    for (document in documents) { //모두 출력
+
+                    // 각 문서를 Chat 객체로 변환하여 리스트에 추가합니다.
+                    for (document in documents) {
                         val chat = document.toObject(Chat::class.java)
                         chats.add(chat)
                     }
+                    // 어댑터에 가져온 채팅 목록 설정
                     chatAdapter.setChats(chats)
                 }
                 .addOnFailureListener { e ->
-                    Log.e("ChatFragment", "Error retrieving messages", e)
+                    // 메시지 불러오기 실패 시 로그에 오류 메시지 출력
+                    Log.e("ChatFragment", "메시지 error", e)
                 }
         }
     }
