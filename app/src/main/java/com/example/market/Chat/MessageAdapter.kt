fb76c4044a9bfa96efa719179d59eae45efa00ec
estherP23 <123536492+estherP23@users.noreply.github.com>
2023-12-06 00:37:20 +0900

    Update MessageAdapter.kt

--- app/src/main/java/com/example/market/Chat/MessageAdapter.kt
+++ app/src/main/java/com/example/market/Chat/MessageAdapter.kt
@@ -10,28 +10,32 @@ import com.example.market.R
 class MessageAdapter(private var messages: List<Message>) :
     RecyclerView.Adapter<MessageAdapter.MessageViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessageViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.message_item, parent, false)
-        return MessageViewHolder(view)
-    }
 
     override fun onBindViewHolder(holder: MessageViewHolder, position: Int) {
         val message = messages[position]
         holder.bind(message)
     }
 
-    override fun getItemCount(): Int {
-        return messages.size
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessageViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.user, parent, false)
+        return MessageViewHolder(view)
     }
+
+
 
     fun setMessages(messages: List<Message>) {
         this.messages = messages
         notifyDataSetChanged()
     }
 
+    override fun getItemCount(): Int {
+        return messages.size
+    }
+
     class MessageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val purchaseTextView: TextView = itemView.findViewById(R.id.purchaseTextView)
         private val messageTextView: TextView = itemView.findViewById(R.id.messageTextView)
+        private val purchaseTextView: TextView = itemView.findViewById(R.id.purchaseTextView)
+
 
         fun bind(message: Message) {
             purchaseTextView.text = message.purchase
