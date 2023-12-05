63d1b3daef16adfdaf9b107a1c377d9da9ff9758
dain0l <123536537+dain0l@users.noreply.github.com>
2023-12-06 00:59:33 +0900

    ProductAdapter.kt

--- app/src/main/java/com/example/market/Home/ProductAdapter.kt
+++ app/src/main/java/com/example/market/Home/ProductAdapter.kt
@@ -12,38 +12,38 @@ import com.example.market.R
 
 
 class ProductAdapter(
-    
+
     private val context: Context,
-    private var productList: List<Product>, 
-    private val navController: NavController, 
+    private var productList: List<Products>,
+    private val navController: NavController,
     private val fromProfile: Boolean
-)  
+)
     : RecyclerView.Adapter<ProductViewHolder>() {
 
-   
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.product_item, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.product, parent, false)
         return ProductViewHolder(view, fromProfile)
     }
 
-   
+
     override fun onBindViewHolder(holder: ProductViewHolder, position: Int) {
-       
+
         val product = productList[position]
-       
-        Glide.with(context).load(product.imageUrl).error(R.drawable.ic_default_image).into(holder.imageView)
-        
-        holder.titleView.text = product.title
-       
+
+        Glide.with(context).load(product.urlImg).error(R.drawable.ic_default_image).into(holder.imageView)
+
+        holder.titleView.text = product.titleName
+
         holder.priceView.text = product.price.toString()
         holder.statusView.text = if (product.sell == true) "🟢 판매중" else "🔴 판매완료"
-        
+
         holder.bind(product, context, navController)
     }
 
     override fun getItemCount() = productList.size
 
-    fun setData(newList: List<Product>) {
+    fun setData(newList: List<Products>) {
         productList = newList
         notifyDataSetChanged()
         Log.d("ProductAdapter", "productList: $productList")
